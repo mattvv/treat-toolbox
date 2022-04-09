d67a4871cd876cd4cbf19343fb1ef2d9ce12faae
Matt Van <mattvan@hey.com>
2022-04-08 20:54:10 -0400

    buggy as fuck

--- components/Forms/Trait/index.tsx
+++ components/Forms/Trait/index.tsx
@@ -233,7 +233,7 @@ export const TraitForm: React.FC<Props> = ({
                         id={"traitSet-" + traitSet.id}
                         {...register(`traitSetIds`, { value: [] })}
                         className="traitSetCheckbox shadow-sm sm:text-sm rounded-md border-transparent inline-block mr-2"
-                        defaultChecked={trait?.traitSetIds?.includes(
+                        defaultChecked={(trait?.traitSetIds || []).includes(
                           traitSet.id
                         )}
                         value={traitSet.id}
